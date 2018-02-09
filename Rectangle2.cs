0b9c9aeb01a00b74d5da3e790460a7d28edc1534
jbernoski2013 <jbernoski2013@conestogac.on.ca>
2018-02-09 10:00:41 -0500

    Completed Rectangle Class

---
+++ Rectangle2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JBQA1
+{
+    class Rectangle
+    {
+        public int getLength(int length)
+        {
+            if (length == 0)
+            {
+                length = 1;
+            }
+
+            return length;
+        }
+
+        public int setLength(int length)
+        {
+            if (length < 1)
+            {
+                length = 1;
+            }
+            return length;
+        }
+
+        public int getWidth(int width)
+        {
+            if (width == 0)
+            {
+                width = 1;
+            }
+            return width;
+        }
+
+        public int setWidth(int width)
+        {
+            if (width < 1)
+            {
+                width = 1;
+            }
+            return width;
+        }
+
+        public int getPerimeter(int length, int width)
+        {
+            return 2 * (length * width);
+        }
+
+        public int getArea(int length, int width)
+        {
+            return width * length;
+        }
+
+    }
+}
